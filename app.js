b1dca1045ff694007ba2e11a194957d5fefe11b2
JessMitch <jessicamitch42@gmail.com>
2016-07-25 20:49:52 +0000

    exercise 5- expressjs workshop

--- app.js
+++ app.js
@@ -13,32 +13,24 @@ var express = require('express');
 var app = express();
 // var operator, firstOperand, secondOperand, solution;
 
-app.get('/post', function(req, res) {
+app.get('/createContent', function(req, res) {
     // console.log('request', req.query);
-    redditAPI.get5Posts({}, function(err, result) {
-        console.log(result);
+    res.send(`
+    <form action="/createContent" method="POST">
+        <div>
+            <input type="text" name="url" placeholder="Enter a URL to content">
+        </div>
+        <div>
+            <input type="text" name="title" placeholder="Enter the title of your content">
+        </div>
         
-        function createLi(post){
-            return `
-            <li>
-                <p>Username: ${post.username} Post: ${post.postTitle}</p>
-            </li>
-            `;
-        }
-        
-        res.send(
-            `
-    <div id="contents">
-        <h1>5 Posts from User # ${result[0].userId}</h1>
-            <ul>
-                ${result.map(function(post){
-                    return createLi(post);
-                }).join("")}
-            </ul>
-    </div>
-`);
-    });
+        <button type="submit">Create!</button>
+    </form>
+    `);
 });
+app.post('/createContent', function(req, res) {
+    res.send("Thank you!")
+})
 
 
 /* YOU DON'T HAVE TO CHANGE ANYTHING BELOW THIS LINE :) */
@@ -50,39 +42,22 @@ var server = app.listen(process.env.PORT, process.env.IP, function () {
   console.log('Example app listening at http://%s:%s', host, port);
 });
 
+/*
+In this exercise, we're going to use Express to simply send an HTML file to our 
+user containing a <form>. To do this, let's write a little HTML file that looks 
+like this:
 
-/*Exercise 4: Retrieving data from our database
-
-Before doing this exercise, go back to your reddit clone MySQL database from the 
-CLI. Using a few INSERT statements, put up a few posts in the posts table. Have 
-at least 10-15 posts in there with various titles and urls. For the userId, set 
-it to 1. Also create a user with ID 1 and call him John Smith or something.
-
-Once you have inserted a few posts in the database, it's now time to retrieve 
-the contents from our web server and display them to the user using an HTML 
-<ul> list with a bunch of <li>s.
-
-Using something similar to your getHomepage function, or even directly the 
-function itself, retrieve the latest 5 posts by createdAt date, including the 
-username who created the content.
-
-Once you have the query, create an endpoint in your Express server which will 
-respond to GET requests to /posts. The Express server will use the MySQL query 
-function to retrieve the array of contents. Then, you should build a string of 
-HTML that you will send with the request.send function.
-
-
-<div id="contents">
-  <h1>List of contents</h1>
-  <ul class="contents-list">
-    <li class="content-item">
-      <h2 class="content-item__title">
-        <a href="http://the.post.url.value/">The content title</a>
-      </h2>
-      <p>Created by CONTENT AUTHOR USERNAME</p>
-    </li>
-    ... one <li> per content that your Sequelize query found
-  </ul>
-</div>
-
+<form action="/createContent" method="POST"> <!-- what is this method="POST" thing? you should know, or ask me :) -->
+  <div>
+    <input type="text" name="url" placeholder="Enter a URL to content">
+  </div>
+  <div>
+    <input type="text" name="title" placeholder="Enter the title of your content">
+  </div>
+  <button type="submit">Create!</button>
+</form>
+You can use template strings (with backticks) to write the HTML code directly in 
+your web server file on multiple lines. Then, using ExpressJS create a GET 
+endpoint called createContent. When someone requests this URL, send the HTML 
+form to them.
 */
