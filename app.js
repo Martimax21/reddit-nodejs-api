f4cb79b5d3a53b37650c839a698945c3ca8a19de
JessMitch <jessicamitch42@gmail.com>
2016-07-25 17:49:30 +0000

    exercise 2- expressjs workshop

--- app.js
+++ app.js
@@ -3,8 +3,9 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('<h1>Hello World!</h1>');
+app.get('/hello', function (req, res) {
+    // console.log('request', req.query);
+  res.send(`<h1>Hello ${req.query.name}!</h1>`);
 });
 
 
@@ -20,3 +21,11 @@ var server = app.listen(process.env.PORT, process.env.IP, function () {
 
   console.log('Example app listening at http://%s:%s', host, port);
 });
+
+
+/*Exercise 2: A wild parameter has appeared!
+
+Create a web server that can listen to requests for /hello?name=firstName, 
+and respond with some HTML that says <h1>Hello _name_!</h1>. For example, 
+if a client requests /hello/John, the server should respond with 
+<h1>Hello John!</h1>.*/
